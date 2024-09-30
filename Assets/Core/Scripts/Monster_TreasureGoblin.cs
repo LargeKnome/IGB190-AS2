adb3ac1b56e3f8c90cd3692964f5f5c2bed29a9e
Scott B <94416375+sctTafe@users.noreply.github.com>
2024-09-30 16:51:23 +1000

    Treasure Goblin - Basics (Done & Tested)
    To Do - Only Trigger Flee Mode when player is close enough to have seen it
    Issue Note - linear path generation, wall issues

--- Assets/Core/Scripts/Monster_TreasureGoblin.cs
+++ Assets/Core/Scripts/Monster_TreasureGoblin.cs
@@ -4,5 +4,40 @@ using UnityEngine;
 
 public class Monster_TreasureGoblin : Monster
 {
+    private bool isEngagedWithPlayer = false;
+    private bool isGoldSpawning_TimerTrigger = false;
+    private bool isGoldSpawning_Movement = false;
 
+    //protected override void Start()
+    //{
+    //    base.Start();
+    //}
+
+    protected override void Update()
+    {
+        base.Update();
+        Update_SpawnGoldTrail();
+    }
+
+
+    private void Update_SpawnGoldTrail()
+    {   
+        // Only Work if moving (Stops a random pile of gold building up)
+        if(agentNavigation.velocity.magnitude > 0.1f)
+            isGoldSpawning_Movement = true;
+        else 
+            isGoldSpawning_Movement = false;
+
+        if (isGoldSpawning_Movement && !isGoldSpawning_TimerTrigger)
+            StartCoroutine(SpawnObjectWithDelay());
+    }
+    IEnumerator SpawnObjectWithDelay()
+    {
+        isGoldSpawning_TimerTrigger = true; 
+        yield return new WaitForSeconds(2f);
+        GoldPickup.Spawn(transform.position, Mathf.RoundToInt(Random.Range(1f, 10f) * goldModifier));
+        isGoldSpawning_TimerTrigger = false;
+    }
+
+    
 }
