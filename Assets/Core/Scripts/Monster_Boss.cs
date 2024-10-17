2ff7279e64ba1267a7dfaf58053eb34f537945fc
Scott B <94416375+sctTafe@users.noreply.github.com>
2024-10-17 14:01:18 +1000

    Boss Fix - Use Buffs as triggers

--- Assets/Core/Scripts/Monster_Boss.cs
+++ Assets/Core/Scripts/Monster_Boss.cs
@@ -11,23 +11,55 @@ public class Monster_Boss : Monster
 
     [SerializeField] private bool _isInSecondStage;
 
+    private bool isPhaseActive;
 
+    private void OnUnitDamaged(GameEvents.OnUnitDamagedInfo damageInfo) 
+    {
+        if (damageInfo.damagedUnit == this && health < stats.GetValue(Stat.MaxHealth) * 0.5f) 
+        {
+            isPhaseActive = true;
+        }
+        else if (damageInfo.damagedUnit == this && health < stats.GetValue(Stat.MaxHealth) * 0.25f) 
+        {
+            isPhaseActive = false;
+        }
+    }
+    
     protected override void Start()
     {
         base.Start();
         engagementState = EngagementState.MoveTowards;
+
+        GameManager.events.OnUnitDamaged.AddListener(OnUnitDamaged);
     }
 
     protected override void Update()
     {
         base.Update();
 
-        Update_CheckIfInSecondStage();
+        //Update_CheckIfInSecondStage();
 
+        if (InPhase2() && !InPhase3()) {
+            _isInSecondStage = true;
+            print("In Seconmd Stage!");
+        }
+        else if(!InPhase2() || InPhase3()) {
+            _isInSecondStage = false;
+            print("In 1st or 3 Stage!");         
+        }
+            
         if (_isInSecondStage) {
             Update_CatAndMouseMode();
-        }      
+        }
     }
+
+    public bool InPhase2() {
+        return stats.HasBuffWithLabel("Phase2");
+    }
+    public bool InPhase3() {
+        return stats.HasBuffWithLabel("Phase3");
+    }
+
 
     private void OnDrawGizmos()
     {
